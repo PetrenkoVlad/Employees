fcc3fa63a97a861c7021339f3bf826992b776825
Vladislav <petrenkovlad2020@gmail.com>
2022-09-03 18:15:28 +0300

    Add project files.

---
+++ EmployeesWeb/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
